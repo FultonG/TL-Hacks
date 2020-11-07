9cc4ad74be455729f6d41a3c804a5ddd6cc1976b
KaylingW <kwong028@fiu.edu>
2020-11-07 18:59:42 -0500

    Tournaments

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 const API = {
   registerUser: (data) => axios.post('/api/auth/create', data),
   authenticateUser: (data) => axios.post('/api/auth/login', data),
-  getSummonerInfo: (summonerName, token) => axios.post('/api/summoner/info', {summonerName}, { headers: {'x-access-token': token}})
+  getSummonerInfo: (summonerName, token) => axios.post('/api/summoner/info', {summonerName}, { headers: {'x-access-token': token}}),
+  getTournamentInfo: () => axios.post('/api/liquidpedia/tournaments')
 }
 
 export default API;
