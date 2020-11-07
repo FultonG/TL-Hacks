b58419f55fa3b40e971ff454e17e0ed25051e74c
Fulton Garcia <fultongarcia@gmail.com>
2020-11-07 12:37:08 -0500

    working mongocharts

--- client/src/Pages/Metrics.js
+++ client/src/Pages/Metrics.js
@@ -1,27 +1,26 @@
-import Card from "../Components/Card";
 import Container from "../Components/Container";
-import { Title, Text } from "../Components/Text";
-import Team from '../svg/TL.svg';
-import React from 'react';
+import React, { createRef, useEffect } from 'react';
 import "regenerator-runtime/runtime";
 import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
-import { contains } from "jquery";
 const Metrics = () => {
-  
-  async function renderChart() {
+  let chartRef = createRef();
+  useEffect(() => {
+    renderChart(chartRef).catch((e) => window.alert(e.message));
+  })
+  async function renderChart(chartRef) {
     const sdk = new ChartsEmbedSDK({
       baseUrl: "https://charts.mongodb.com/charts-tl-hacks-rbiuz"
     });
-  
+
     const chart = sdk.createChart({
       chartId: "21e0df07-5172-43ef-801d-122c85b97a14"
     });
-    await chart.render(document.getElementById("chart"));
-  
+
+    await chart.render(chartRef.current);
+
   }
-  renderChart().catch((e) => window.alert(e.message));
-return (
- <Container align="center" justify="center" height="90%"></Container>
-)
+  return (
+    <Container align="center" justify="center" height="90%" ref={chartRef}></Container>
+  )
 };
-  export default Metrics;
+export default Metrics;
