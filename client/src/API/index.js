c585f7fc7780189df4d209ed46bdd34f72d86897
KaylingW <kwong028@fiu.edu>
2020-11-07 23:55:32 -0500

    poggies

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -3,7 +3,7 @@ const API = {
   registerUser: (data) => axios.post('/api/auth/create', data),
   authenticateUser: (data) => axios.post('/api/auth/login', data),
   getSummonerInfo: (summonerName, token) => axios.post('/api/summoner/info', {summonerName}, { headers: {'x-access-token': token}}),
-  getTournamentInfo: () => axios.post('/api/liquidpedia/tournaments')
+  getTournamentInfo: () => axios.get('/api/liquidpedia/tournaments')
 }
 
 export default API;
