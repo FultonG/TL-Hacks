2173f817de093e0f406073c9d9f1608fa4b6afa2
Fulton Garcia <fultongarcia@gmail.com>
2020-11-08 15:15:22 -0500

    NLP

--- controllers/healthCheck.js
+++ controllers/healthCheck.js
@@ -1,12 +1,25 @@
 const client = require('../db');
+const language = require('@google-cloud/language');
 
 const healthCheck = {
   addHealthCheck: async (data) => {
     try {
       let db = await client;
       let collection = db.collection('healthCheck');
-      let res = await collection.updateOne({ uId: data.uId, summonerName: data.summonerName }, {$set: {...data}}, {upsert: true});
-      return {status: 200, data: res};
+
+      // Creates a client
+      const client = new language.LanguageServiceClient();
+      const document = {
+        content: text,
+        type: 'PLAIN_TEXT',
+      };
+
+      // Detects the sentiment of the document
+      const [result] = await client.analyzeSentiment({ document });
+
+      const sentiment = result.documentSentiment;
+      let res = await collection.updateOne({ uId: data.uId, summonerName: data.summonerName }, { $set: { ...data, sentimentScore: sentiment.score } }, { upsert: true });
+      return { status: 200, data: res };
     } catch (e) {
       console.log(e)
       return { status: 500, data: e.message };
