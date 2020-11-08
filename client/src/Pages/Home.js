d6bf773f0010ad3d8f3fc44d5e2799e114a07d07
jacob baggs <jacobbaggs@me.com>
2020-11-07 21:16:02 -0500

    adding the who what why

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,32 +1,75 @@
 import Card from "../Components/Card";
 import Container from "../Components/Container";
 import { Title, Text } from "../Components/Text";
-import Team from '../svg/TL.svg';
-
+import Team from "../svg/TL.svg";
+import What from "../svg/what.png";
+import Who from "../svg/who.png";
+import Why from "../svg/why.png";
 
 const Home = () => {
   return (
     <Container align="center" direction="column" height="90%">
-      <Container justify="center" align="center" direction="column" height="60%">
+      <Container
+        justify="center"
+        align="center"
+        direction="column"
+        height="60%"
+      >
         <Title>Esports Training Camp XD</Title>
-        <Text>Insert Slogan Here ???</Text>
+        <Text>Train and Prepare Like a Olympian ???</Text>
       </Container>
       <Container justify="space-evenly" align="center" height="35%">
-        <Container width="30%" direction="column" justify="center" align="center">
-          <Card height="80%" margin="10px" radius="25px" hover cursor="pointer"></Card>
-          <Text>Placeholder</Text>
+        <Container
+          width="30%"
+          direction="column"
+          justify="center"
+          align="center"
+        >
+          <Card
+            height="80%"
+            margin="10px"
+            radius="25px"
+            hover
+            cursor="pointer"
+            background={`url(${Who})  no-repeat`}
+          ></Card>
+          <Text>Who</Text>
         </Container>
-        <Container width="30%" direction="column" justify="center" align="center">
-          <Card height="80%" margin="10px" radius="25px" background={`url(${Team}) no-repeat`} hover cursor="pointer"></Card>
-          <Text>Placeholder</Text>
+        <Container
+          width="30%"
+          direction="column"
+          justify="center"
+          align="center"
+        >
+          <Card
+            height="80%"
+            margin="10px"
+            radius="25px"
+            background={`url(${What}) no-repeat`}
+            hover
+            cursor="pointer"
+          ></Card>
+          <Text>What</Text>
         </Container>
-        <Container width="30%" direction="column" justify="center" align="center">
-          <Card height="80%" margin="10px" radius="25px" hover cursor="pointer"></Card>
-          <Text>Placeholder</Text>
+        <Container
+          width="30%"
+          direction="column"
+          justify="center"
+          align="center"
+        >
+          <Card
+            height="80%"
+            margin="10px"
+            radius="25px"
+            hover
+            cursor="pointer"
+            background={`url(${Why}) no-repeat`}
+          ></Card>
+          <Text>Why</Text>
         </Container>
       </Container>
     </Container>
-  )
+  );
 };
 
 export default Home;
